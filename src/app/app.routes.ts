f74a498e0c463ab285ddf48d7d46953f77b05e7c
arda <ipek.arda.pro@gmail.com>
2024-06-25 14:15:20 +0200

    basic navigation example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ProductComponent} from "./product/product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'products', component: ProductComponent}
+];
