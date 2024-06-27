501ce8b854d8fecde63bc47393cf0aa65d62eb8b
decarvalho <decarvalho.dev@gmail.com>
2024-06-27 11:56:18 +0200

    add client component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import {ProductComponent} from "./product/product.component";
+import { ProductComponent } from "./product/product.component";
+import { ClientComponent } from './client/client.component';
 
 export const routes: Routes = [
-  { path: 'products', component: ProductComponent}
+  { path: 'products', component: ProductComponent },
+  { path: 'clients', component: ClientComponent }
 ];
